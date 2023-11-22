8c2729cadc29ea66c5e58978a1fd490783493a45
busynovadad <bmckuhen@yahoo.com>
2023-11-21 19:50:38 -0500

    inital upload of images updates to main.py

--- main.py
+++ main.py
@@ -6,14 +6,121 @@ from os import listdir
 from os.path import isfile, join
 pygame.init()
 pygame.display.set_caption("platformer!")
-BG_COLOR = (255,255,255)
+# BG_COLOR = (255,255,255)
 WIDTH, HEIGHT = 1000,800
 FPS = 60
 PLAYER_VEL = 5
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 
+
+def flip(sprites):
+    return [pygame.transform.flip(sprite,True, False) for sprite in sprites]
+
+
+def load_sprite_sheets(dir1, dir2, width, height, direction = False):
+    path = join("assets", dir1, dir2)
+    images = [f for f in listdir(path) if isfile(join(path, f))]
+
+    all_sprites = {}
+    for image in images:
+        sprite_sheet = pygame.image.load(join(path, image)).convert_alpha()
+        sprites = []
+        for i in range(sprite_sheet.get_width() // width):
+            surface = pygame.Surface((width, height), pygame.SRCALPHA, 32)
+            rect = pygame.Rect(i * width, 0, width, height)
+            surface.blit(sprite_sheet, (0,0), rect)
+            sprites.append(pygame.transform.scale2x(surface))
+
+        if direction:
+            all_sprites[image.replace(".png","") + "_right"] = sprites
+            all_sprites[image.replace(".png","") + "_left"] = sprites
+        else:
+            all_sprites[image.replace(".png","")] = sprites
+
+    return all_sprites
+
+            
+
+class Player(pygame.sprite.Sprite):
+    COLOR = (255,0,0)
+    GRAVITY = 1
+    SPRITES = load_sprite_sheets("MainCharacters", "MaskDude", 32, 32)
+
+    def __init__(self,x,y,width,height):
+        self.rect = pygame.Rect(x,y,width,height)
+        self.x_vel = 0
+        self.y_vel = 0
+        self.mask = None
+        self.direction = "left"
+        self.animation_count = 0
+        self.fall_count = 0
+    
+    def move(self, dx, dy):
+        self.rect.x += dx
+        self.rect.y += dy
+
+    def move_left(self, vel):
+        self.x_vel = -vel
+        if self.direction != "left":
+            self.direction = "left"
+            self.animation_count = 0
+
+    def move_right(self, vel):
+        self.x_vel = vel
+        if self.direction != "right":
+            self.direction = "right"
+            self.animation_count = 0
+
+    def loop(self, fps):
+        self.y_vel += min(1, (self.fall_count / fps) * self.GRAVITY)
+        self.move(self.x_vel, self.y_vel)
+        self.fall_count += 1
+
+    def draw(self, win):
+        pygame.draw.rect(win, self.COLOR, self.rect)
+
+
+def get_background(bgname):
+    image = pygame.image.load(join("assets","Background",bgname))
+    _, _, bgwidth, bgheight = image.get_rect()
+    tiles = []
+
+    for i in range(WIDTH // bgwidth + 1):
+        for j in range(HEIGHT // bgheight + 1):
+            pos = (i * bgwidth, j*bgheight)
+            tiles.append(pos)
+    
+    return tiles, image
+
+
+def draw(window, background, bg_image, player):
+    for tile in background:
+        window.blit(bg_image, tile)
+
+    player.draw(window)
+    pygame.display.update()
+
+def handle_move(player):
+    keys = pygame.key.get_pressed()
+    player.x_vel = 0
+
+
+    if keys[pygame.K_LEFT]:
+        player.move_left(PLAYER_VEL)
+
+
+    # print('1')
+    if keys[pygame.K_RIGHT]:
+        # print('2')
+        player.move_right(PLAYER_VEL)
+
+
+
 def main(window):
     clock = pygame.time.Clock()
+    background, bg_image = get_background("Blue.png")
+
+    player = Player(100, 100, 50, 50)
 
     run = True
     while run:
@@ -23,6 +130,11 @@ def main(window):
             if event.type == pygame.QUIT:
                 run = False
                 break
+        
+        player.loop(FPS)
+        handle_move(player)
+        draw(window, background, bg_image, player)
+
 
     pygame.quit()
     quit()
