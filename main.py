45309ccfd6f58da2ddb435ccc68407e76a935635
busynovadad <bmckuhen@yahoo.com>
2023-11-26 11:45:00 -0500

    added Fire to main; adding traps

--- main.py
+++ main.py
@@ -163,6 +163,36 @@ class Block(Object):
         self.mask = pygame.mask.from_surface(self.image)
 
 
+class Fire(Object):
+    ANIMATION_DELAY = 3
+
+    def __init__(self, x, y, width, height):
+        super().__init__(x, y, width, height, "fire")
+        self.fire = load_sprite_sheets("Traps", "Fire", width, height)
+        self.image = self.fire["off"][0]
+        self.mask = pygame.mask.from_surface(self.image)
+        self.animation_count = 0
+        self.animation_name = "off"
+
+    def on(self):
+        self.animation_name="on"
+
+    def off(self):
+        self.animation_name="off"
+
+    def loop(self):
+        # sprite_sheet_name = sprite_sheet # + "_" + self.direction
+        # sprites = self.SPRITES[sprite_sheet_name]
+        sprites = self.fire[self.animation_name]
+        sprite_index = (self.animation_count // self.ANIMATION_DELAY) % len(sprites)
+        self.image = sprites[sprite_index]
+        self.animation_count += 1
+        self.rect = self.sprite.get_rect(topleft=(self.rect.x, self.rect.y))
+        self.mask = pygame.mask.from_surface(self.sprite)
+
+        if self.animation_count // self.ANIMATION_DELAY > len(sprites):
+            self.animation_count = 0
+
 
 def get_background(bgname):
     image = pygame.image.load(join("assets","Background",bgname))
@@ -242,9 +272,11 @@ def main(window):
     block_size = 96
 
     player = Player(100, 100, 50, 50)
+    fire = Fire(100, HEIGHT - block_size - 64, 32, 64)
+    fire.on()
     floor = [Block(i * block_size, HEIGHT - block_size, block_size) for i in range(-WIDTH // block_size, WIDTH * 2 // block_size)]
     objects = [*floor, Block(0, HEIGHT - block_size * 2, block_size),
-               Block(block_size * 3, HEIGHT - block_size * 4, block_size)]
+               Block(block_size * 3, HEIGHT - block_size * 4, block_size), fire]
     # blocks = [Block(0, HEIGHT - block_size, block_size)]
     offset_x = 0
     scroll_area_width = 200
